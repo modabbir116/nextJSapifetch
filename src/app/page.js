6e3eb3179c1b7560479cfc348c8c27fc682330d8
unknown <modabber.web@gmail.com>
2025-10-08 10:51:54 +0600

    UI design done project done

--- src/app/page.js
+++ src/app/page.js
@@ -210,3 +210,26 @@ export default function Home() {
     </div>
   );
 }
+
+
+// "use client" 
+// import Container from "@/app/components/Layouts/Container.js"; 
+// import { useParams } from "next/navigation"; 
+// import { useEffect, useState } from "react";  
+// export default function singleBlog(){
+// const [singleBlogData, setSingleBlogData] = useState({})     
+// const {id} = useParams();     console.log(id);      useEffect(()=>{         
+// fetch(https://dummyjson.com/posts/${id})         
+// .then((res) => res.json())         
+// .then((data) => setSingleBlogData(data))       
+// }, [])
+//  console.log(singleBlogData);            
+// return(         
+// <div>             
+// <Container>                 
+// <div>  BLog ID: {id}                     
+// <h2>Title: {singleBlogData.title}</h2>                     
+// <hp>Body: {singleBlogData.body}</hp>                 
+// </div>             
+// </Container>         
+// </div>     ) }
